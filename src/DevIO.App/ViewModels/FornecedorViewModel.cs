ba050d8bb2c74f1cb3362b6eac95ff27ce41227d
Guilherme de Alvarenga <gui.alva50@gmail.com>
2019-09-02 14:19:15 -0300

    Customização das Views

--- src/DevIO.App/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.App/ViewModels/FornecedorViewModel.cs
@@ -15,6 +15,7 @@ namespace DevIO.App.ViewModels
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
 
         [DisplayName("Tipo")]
         public int TipoFornecedor { get; set; }
